d0585455d571533c2295f2069f5663c2242ee970
Pritchard Oluwafeyisanumi <104890172+Pritchard94@users.noreply.github.com>
2023-03-28 13:41:09 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import Navbar from 'snap/src/Components/Navbar.jsx'
-import Hero from 'snap/src/Components/hero.jsx'
+import Navbar from '/Users/bigmac/Desktop/Programming/Snap/snap/src/Components/Navbar.jsx'
+import Hero from '/Users/bigmac/Desktop/Programming/Snap/snap/src/Components/hero.jsx'
 
 
 
